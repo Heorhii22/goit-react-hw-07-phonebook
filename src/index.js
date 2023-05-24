d510b3c576fe9003fa0e597f0a399047a4d9c6ad
George Artemov <113346839+Heorhii22@users.noreply.github.com>
2023-05-24 22:21:22 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+import { store } from 'components/Redux/Store/store';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { Provider } from 'react-redux';
+import { App } from './components/App/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
